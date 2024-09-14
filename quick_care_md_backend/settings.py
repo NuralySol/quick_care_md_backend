11cd1597c9a5b53758f3af76d0be408d0664c9a9
NuralySol <soltonbekov@gmail.com>
2024-09-14 15:12:47 -0400

    Add corsheaders to INSTALLED_APPS and enable CORS middleware

--- quick_care_md_backend/settings.py
+++ quick_care_md_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
     'main_app',  # Add your main application
 ]
 
@@ -56,6 +57,7 @@ REST_FRAMEWORK = {
 
 # Middleware configuration
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'quick_care_md_backend.urls'
 
